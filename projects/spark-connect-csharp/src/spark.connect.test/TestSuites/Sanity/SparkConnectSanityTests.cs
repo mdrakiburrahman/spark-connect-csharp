169128d9b1d6a9e834b1c5e20893797324feee2f
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-04-19 11:46:55 -0400

    Added test fixtures

---
+++ projects/spark-connect-csharp/src/spark.connect.test/TestSuites/Sanity/SparkConnectSanityTests.cs
@@ -0,0 +1,89 @@
+// -----------------------------------------------------------------------------
+// <summary>
+// Contains Basic Sanity Tests for Spark Connect.
+// </summary>
+//
+// <copyright file="SparkConnectSanityTests.cs" company="Apache Software Foundation (ASF)">
+// Copyright (c) Apache Software Foundation (ASF). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Spark.Connect.Test.TestSuites.Sanity
+{
+    /// <summary>
+    /// Spark Connect Sanity tests.
+    /// </summary>
+    [TestClass]
+    public class SparkConnectSanityTests
+    {
+        #region Test Properties
+
+        private const string TestContainerImage = "testcontainers/helloworld:1.1.0";
+
+        #endregion Test Properties
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SparkConnectSanityTests"/> class.
+        /// </summary>
+        public SparkConnectSanityTests() { }
+
+        #region Class scoped fixture
+
+        /// <summary>
+        /// This method runs on per Test Class spinup.
+        /// </summary>
+        /// <param name="context">The test context.</param>
+        [ClassInitialize]
+        public static void SparkConnectSanityTestsInit(TestContext context)
+        {
+            Console.WriteLine($"Setting up class scoped fixture for test {context.TestName}");
+        }
+
+        /// <summary>
+        /// This method runs on per Test Class Teardown.
+        /// </summary>
+        [ClassCleanup]
+        public static void SqlServerDatabaseTestsClassTeardown()
+        {
+            Console.WriteLine("Tearing down class scoped fixture");
+        }
+
+        #endregion Class scoped fixture
+
+        #region Test scoped fixture
+
+        /// <summary>
+        /// This method runs on per Test Method spinup.
+        /// </summary>
+        [TestInitialize]
+        public void SqlServerDatabaseTestsMethodInit()
+        {
+            Console.WriteLine("Setting up method scoped fixture");
+        }
+
+        /// <summary>
+        /// This method runs on per Test Method teardown.
+        /// </summary>
+        [TestCleanup]
+        public void SqlServerDatabaseTestsMethodTeardown()
+        {
+            Console.WriteLine("Tearing down method scoped fixture");
+        }
+
+        #endregion Test scoped fixture
+
+        #region Test Methods
+
+        /// <summary>
+        /// Ensure Spark Connect can connect to the Spark cluster.
+        /// </summary>
+        [TestMethod]
+        public void SparkConnectCanConnect()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        #endregion Test Methods
+    }
+}
