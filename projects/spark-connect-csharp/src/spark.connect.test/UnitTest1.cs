8b4733ce1b35a734176db10a9438f9ec60301293
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-04-19 11:11:00 -0400

    Added Stylecop

--- projects/spark-connect-csharp/src/spark.connect.test/UnitTest1.cs
+++ projects/spark-connect-csharp/src/spark.connect.test/UnitTest1.cs
@@ -1,3 +1,14 @@
+// -----------------------------------------------------------------------------
+// <summary>
+// TODO - The unit test class.
+// </summary>
+//
+// <copyright file="UnitTest1.cs" company="Apache Software Foundation (ASF)">
+// Copyright (c) Apache Software Foundation (ASF). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------------
+
 namespace Spark.Connect.Test
 {
     /// <summary>
