e92728ab014f523b90b9898970d38d48fc605838
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-04-20 10:14:08 -0400

    New C# project, added IDataFrameWriter

---
+++ projects/spark-connect-csharp/src/spark.connect.core/Sql/DataFrame/Writer/IDataFrameWriter.cs
@@ -0,0 +1,39 @@
+// -----------------------------------------------------------------------------
+// <summary>
+// A contract for writing data frames.
+// </summary>
+//
+// <copyright file="IDataFrameWriter.cs" company="Apache Software Foundation (ASF)">
+// Copyright (c) Apache Software Foundation (ASF). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Spark.Connect.Core.Sql.DataFrame.Writer
+{
+    /// <summary>
+    /// A contract for writing data frames.
+    /// </summary>
+    public interface IDataFrameWriter
+    {
+        /// <summary>
+        /// Specifies the saving mode for the data, e.g. Append, Overwrite, ErrorIfExists.
+        /// </summary>
+        /// <param name="saveMode">The saving mode for the data.</param>
+        /// <returns>The data frame writer.</returns>
+        IDataFrameWriter Mode(string saveMode);
+
+        /// <summary>
+        /// Specifies the data format (data source type) for the underlying data, e.g. parquet.
+        /// </summary>
+        /// <param name="source">The data format for the underlying data.</param>
+        /// <returns>The data frame writer.</returns>
+        IDataFrameWriter Format(string source);
+
+        /// <summary>
+        /// Writes the data frame to the given path.
+        /// </summary>
+        /// <param name="path">The path to write the data frame to.</param>
+        void Save(string path);
+    }
+}
