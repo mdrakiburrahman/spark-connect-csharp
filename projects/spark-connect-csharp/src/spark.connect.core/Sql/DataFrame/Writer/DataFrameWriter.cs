5b0f055aec31d971cbef5e486f0cd489d20091df
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-04-20 11:42:18 -0400

    Implemented a couple methods on DataFrameWriter

--- projects/spark-connect-csharp/src/spark.connect.core/Sql/DataFrame/Writer/DataFrameWriter.cs
+++ projects/spark-connect-csharp/src/spark.connect.core/Sql/DataFrame/Writer/DataFrameWriter.cs
@@ -9,29 +9,68 @@
 // </copyright>
 // -----------------------------------------------------------------------------
 
+using Spark.Connect.Core.Sql.Session;
+
 namespace Spark.Connect.Core.Sql.DataFrame.Writer
 {
     /// <summary>
     /// DataFrameWriter supports writing data frame to storage.
     /// </summary>
-    public class DataFrameWriter : IDataFrameWriter
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="DataFrameWriter"/> class.
+    /// </remarks>
+    /// <param name="sparkSession">The Spark Session.</param>
+    /// <param name="relation">An explicit relationship type.</param>
+    /// <param name="saveMode">The save mode.</param>
+    /// <param name="formatSource">The format source.</param>
+    public class DataFrameWriter(
+        ISparkSession sparkSession,
+        Relation relation,
+        string saveMode = "",
+        string formatSource = ""
+        ) : IDataFrameWriter
     {
-        /// <inheritdoc/>
-        public IDataFrameWriter Format(string source)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly ISparkSession sparkSession = sparkSession;
+        private readonly Relation relation = relation;
+        private string saveMode = saveMode;
+        private string formatSource = formatSource;
 
         /// <inheritdoc/>
         public IDataFrameWriter Mode(string saveMode)
         {
-            throw new NotImplementedException();
+            this.saveMode = saveMode;
+            return this;
+        }
+
+        /// <inheritdoc/>
+        public IDataFrameWriter Format(string source)
+        {
+            this.formatSource = source;
+            return this;
         }
 
         /// <inheritdoc/>
         public void Save(string path)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the save mode based on the specified mode string.
+        /// </summary>
+        /// <param name="mode">The mode string.</param>
+        /// <returns>The save mode.</returns>
+        private static WriteOperation.Types.SaveMode GetSaveMode(string mode)
+        {
+            return mode switch
+            {
+                "" => WriteOperation.Types.SaveMode.Unspecified,
+                "Append" => WriteOperation.Types.SaveMode.Append,
+                "Overwrite" => WriteOperation.Types.SaveMode.Overwrite,
+                "ErrorIfExists" => WriteOperation.Types.SaveMode.ErrorIfExists,
+                "Ignore" => WriteOperation.Types.SaveMode.Ignore,
+                _ => throw new ArgumentException($"Unsupported save mode: {mode}"),
+            };
         }
     }
 }
