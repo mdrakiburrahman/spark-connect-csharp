9a744940aefba79c9a69b27a636ad87b1700ea00
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-04-20 21:23:55 -0400

    Some more TODOs

--- projects/spark-connect-csharp/src/spark.connect.core/Sql/DataFrame/DataFrame.cs
+++ projects/spark-connect-csharp/src/spark.connect.core/Sql/DataFrame/DataFrame.cs
@@ -9,11 +9,17 @@
 // </copyright>
 // -----------------------------------------------------------------------------
 
+using Apache.Arrow;
+using Apache.Arrow.Ipc;
+using Apache.Arrow.Types;
+
 using Spark.Connect.Core.Sql.DataFrame.Columns;
 using Spark.Connect.Core.Sql.DataFrame.Rows;
 using Spark.Connect.Core.Sql.DataFrame.Types;
 using Spark.Connect.Core.Sql.DataFrame.Writer;
 using Spark.Connect.Core.Sql.Session;
+
+using StructType = Spark.Connect.Core.Sql.DataFrame.Types.StructType;
 
 namespace Spark.Connect.Core.Sql.DataFrame
 {
@@ -115,15 +121,102 @@ namespace Spark.Connect.Core.Sql.DataFrame
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Shows the data in an Arrow batch.
+        /// </summary>
+        /// <param name="arrowBatch">The Arrow batch.</param>
         private void ShowArrowBatch(ExecutePlanResponse.Types.ArrowBatch arrowBatch)
         {
             this.ShowArrowBatchData(arrowBatch.Data.ToByteArray());
         }
 
+        /// <summary>
+        /// Shows the data in an Arrow batch.
+        /// </summary>
+        /// <param name="data">The byte array representing the Arrow batch.</param>
         private void ShowArrowBatchData(byte[] data)
         {
+            var rows = this.ReadArrowBatchData(data, null);
+            foreach (var row in rows)
+            {
+                var values = row.Values();
+                if (values == null)
+                {
+                    throw new Exception("Failed to get values in the row");
+                }
+
+                Console.WriteLine(string.Join(", ", values));
+            }
+        }
+
+        private List<Row> ReadArrowBatchData(byte[] data, StructType? schema)
+        {
             // TODO: Implement
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reads the value of a column from an Arrow record batch.
+        /// </summary>
+        /// <param name="record">The Arrow record batch.</param>
+        /// <param name="columnIndex">The index of the column.</param>
+        /// <param name="values">The array to store the column values.</param>
+        private void ReadArrowRecordColumn(RecordBatch record, int columnIndex, object[][] values)
+        {
+            int numRows = record.Length;
+            var column = record.Column(columnIndex);
+
+            for (int rowIndex = 0; rowIndex < numRows; rowIndex++)
+            {
+                switch (column.Data.DataType.TypeId)
+                {
+                    case ArrowTypeId.Boolean:
+#pragma warning disable CS8601, CS8602 // Possible null reference assignment.
+                        values[rowIndex][columnIndex] = (column as BooleanArray).GetValue(rowIndex);
+                        break;
+                    case ArrowTypeId.Int8:
+                        values[rowIndex][columnIndex] = (column as Int8Array).GetValue(rowIndex);
+                        break;
+                    case ArrowTypeId.Int16:
+                        values[rowIndex][columnIndex] = (column as Int16Array).GetValue(rowIndex);
+                        break;
+                    case ArrowTypeId.Int32:
+                        values[rowIndex][columnIndex] = (column as Int32Array).GetValue(rowIndex);
+                        break;
+                    case ArrowTypeId.Int64:
+                        values[rowIndex][columnIndex] = (column as Int64Array).GetValue(rowIndex);
+                        break;
+                    case ArrowTypeId.Float:
+                        values[rowIndex][columnIndex] = (column as FloatArray).GetValue(rowIndex);
+                        break;
+                    case ArrowTypeId.Double:
+                        values[rowIndex][columnIndex] = (column as DoubleArray).GetValue(rowIndex);
+                        break;
+                    case ArrowTypeId.String:
+                        values[rowIndex][columnIndex] = (column as StringArray).GetString(rowIndex);
+                        break;
+                    case ArrowTypeId.Binary:
+                        values[rowIndex][columnIndex] = (column as BinaryArray)
+                            .GetBytes(rowIndex)
+                            .ToArray();
+                        break;
+                    case ArrowTypeId.Timestamp:
+                        values[rowIndex][columnIndex] = (column as TimestampArray).GetTimestamp(
+                            rowIndex
+                        );
+                        break;
+                    case ArrowTypeId.Date64:
+                        values[rowIndex][columnIndex] = (column as Date64Array).GetDateTimeOffset(
+                            rowIndex
+                        );
+                        break;
+#pragma warning restore CS8601, CS8602 // Possible null reference assignment.
+                    default:
+                        throw new Exception(
+                            $"Unsupported arrow data type {column.Data.DataType.TypeId} in column {columnIndex}"
+                        );
+                }
+            }
         }
     }
 }
